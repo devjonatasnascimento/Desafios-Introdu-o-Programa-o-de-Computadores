94250763c1d8f93a20e735d8b75545011ea11666
Jonatas Nascimento <jonatasrad@outlook.com>
2025-10-16 09:32:44 +0000

    Definição das variáveis

---
+++ super_trunfo.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/*
+O que você vai fazer
+Criar um programa em C que permita ao usuário inserir os dados de duas cartas do Super Trunfo. Para cada carta, o usuário deverá fornecer as seguintes informações:
+Estado: Uma letra de 'A' a 'H' (representando um dos oito estados). Tipo: char
+Código da Carta: A letra do estado seguida de um número de 01 a 04 (ex: A01, B03). Tipo: char[] (um array de caracteres, ou string)
+Nome da Cidade: O nome da cidade. Tipo: char[] (string)
+População: O número de habitantes da cidade. Tipo: int
+Área (em km²): A área da cidade em quilômetros quadrados. Tipo: float
+PIB: O Produto Interno Bruto da cidade. Tipo: float 
+Número de Pontos Turísticos: A quantidade de pontos turísticos na cidade. Tipo: int
+*/
+
+
+
+// Desafio Super Trunfo - Países
+// Tema 1 - Cadastro das cartas
+// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
+
+int main() {
+  // Área para definição das variáveis para armazenar as propriedades das cidades
+  char estado;
+  int codigo_da_carta, populacao, numero_de_pontos_turisticos;
+  char nome_da_cidade[50];
+  float area, pib;
+  
+  // Área para entrada de dados
+ 
+
+  // Área para exibição dos dados da cidade
+
+return 0;
+} 
+
