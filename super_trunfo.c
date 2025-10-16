91ac23eb42f12bce8d4d69ebc7aa8ac5d1f474b3
Jonatas Nascimento <jonatasrad@outlook.com>
2025-10-16 10:17:09 +0000

    Revisão das variaveis e adição das entradas de dados

--- super_trunfo.c
+++ super_trunfo.c
@@ -20,13 +20,28 @@ Número de Pontos Turísticos: A quantidade de pontos turísticos na cidade. Tip
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-  char estado;
-  int codigo_da_carta, populacao, numero_de_pontos_turisticos;
-  char nome_da_cidade[50];
-  float area, pib;
+  char estado1, estado2;
+  int populacao1, populacao2, numero_de_pontos_turisticos1, numero_de_pontos_turisticos2;
+  char codigo_da_carta1[3], codigo_da_carta2[3], nome_da_cidade1[50], nome_da_cidade2[50];
+  float area1, area2, pib1, pib2;
   
   // Área para entrada de dados
- 
+    printf("A seguir serao inseridos os dados de duas cartas super tunfo.\nDigite os dados da primeira carta:\n");
+    printf("Uma letra de A a H que representa o estado 01: ");
+    scanf("%c", &estado1);
+    printf("Codigo da carta com a letra do estado seguida de um número de 01 a 04: ");
+    scanf("%s", codigo_da_carta1);
+    printf("Nome da cidade 01: ");
+    scanf("%s", nome_da_cidade1);
+    printf("Número de habitantes da cidade 01: ");
+    scanf("%d", &populacao1);
+    printf("Área da cidade 01 em quilômetros quadrados: ");
+    scanf("%f", area1);
+    printf("Valor do Produto Interno Bruto (PIB) da cidade 01: ");
+    scanf("%f", pib1);
+    printf("Quantidade de pontos turísticos na cidade 01: ");
+    scanf("%d", &numero_de_pontos_turisticos1);
+
 
   // Área para exibição dos dados da cidade
 
